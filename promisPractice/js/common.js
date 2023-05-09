5c81ab094102e15df326de57ca186b9bc10a5543
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-09 14:34:19 +0900

    callback

--- promisPractice/js/common.js
+++ promisPractice/js/common.js
@@ -10,5 +10,15 @@ setTimeout(function () {
 
 // 1. callback 함수로 a,b,c 순으로 콘솔에 출력
 
+setTimeout(function () {
+  console.log('A')
+  setTimeout(function () {
+    console.log('B')
+    setTimeout(function () {
+      console.log('C')
+    }, 1000);
+  }, 2000);
+}, 3000);
+
 /* 2. promise를 함수로 등록한 뒤 그 함수를 이용하여
       a,b,c 순으로 콘솔에 출력 */
