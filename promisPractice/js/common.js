666a4fe24d58a609b8314410cf357f5a5a3c2050
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-09 14:35:16 +0900

    Update common.js

--- promisPractice/js/common.js
+++ promisPractice/js/common.js
@@ -22,3 +22,21 @@ setTimeout(function () {
 
 /* 2. promise를 함수로 등록한 뒤 그 함수를 이용하여
       a,b,c 순으로 콘솔에 출력 */
+function delay(time) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, time);
+  });
+}
+
+delay(3000)
+  .then(function() {
+    console.log('A');
+    return delay(2000);
+  })
+  .then(function() {
+    console.log('B');
+    return delay(1000);
+  })
+  .then(function() {
+    console.log('C');
+  });
